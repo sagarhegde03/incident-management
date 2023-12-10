69a1185d101c99f5e2c1f441b45ba4f77773a928
Sagar Hegde <sagar.hegde@se.com>
2023-12-10 02:46:24 +0000

    commit to main

---
+++ app/incidents/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'incidents/test/integration/FirstJourney',
+		'incidents/test/integration/pages/IncidentsList',
+		'incidents/test/integration/pages/IncidentsObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, IncidentsList, IncidentsObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('incidents') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheIncidentsList: IncidentsList,
+					onTheIncidentsObjectPage: IncidentsObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
